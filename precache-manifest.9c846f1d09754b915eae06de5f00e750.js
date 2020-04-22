d2370e040f323bad85286287925183fd1c157aed
unknown <ittalentsvahagn@gmail.com>
2020-04-22 22:02:44 +0400

    Updates

---
+++ precache-manifest.9c846f1d09754b915eae06de5f00e750.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29ba6146efada787fd508401efbdd368",
+    "url": "/trainings-app/index.html"
+  },
+  {
+    "revision": "cb697a1a1ac9e5a96d9b",
+    "url": "/trainings-app/static/css/main.e9915ae0.chunk.css"
+  },
+  {
+    "revision": "752d9fc6345ddcf23b4b",
+    "url": "/trainings-app/static/js/2.e7332483.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/trainings-app/static/js/2.e7332483.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb697a1a1ac9e5a96d9b",
+    "url": "/trainings-app/static/js/main.94f1b363.chunk.js"
+  },
+  {
+    "revision": "68b48307649be77d3742",
+    "url": "/trainings-app/static/js/runtime-main.26ed3352.js"
+  }
+]);
